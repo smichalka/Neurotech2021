f696e4c73c6d9574ff0c40471da1b6d0750acc59
Anusha Datar <anushadatar@gmail.com>
2021-05-08 17:07:33 -0400

    Catch post requests on server

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express();
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
 	res.render('index');
+})
+
+app.post('/', function(req, res) {
+	const body = req.body.choice
+	res.set('Content-Type', 'text/plain')
+	res.send('You sent: ${body} to Express')
+	console.log('got post!')
 })
 
 app.listen(port, function() {
